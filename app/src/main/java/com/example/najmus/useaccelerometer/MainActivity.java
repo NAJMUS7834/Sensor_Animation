21ffe787e9dd2b11c91e31577c8ca44f2d2076ce
NAJMUS SEEMAB <najmus.seemab@gmail.co>
2018-04-25 16:12:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/najmus/useaccelerometer/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.najmus.useaccelerometer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private long lastUpdate;
+
+
+    AnimatedView animatedView = null;
+    ShapeDrawable mDrawable = new ShapeDrawable();
+    public static int x;
+    public static int y;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        lastUpdate = System.currentTimeMillis();
+
+        animatedView = new AnimatedView(this);
+        setContentView(animatedView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer,
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // TODO Auto-generated method stub
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            x -= (int) event.values[0];
+            y += (int) event.values[1];
+
+        }
+    }
+
+    public class AnimatedView extends android.support.v7.widget.AppCompatImageView {
+
+        static final int width = 50;
+        static final int height = 50;
+
+        public AnimatedView(Context context) {
+            super(context);
+            // TODO Auto-generated constructor stub
+
+            mDrawable = new ShapeDrawable(new OvalShape());
+            mDrawable.getPaint().setColor(0xffffAC23);
+            mDrawable.setBounds(x, y, x + width, y + height);
+
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+
+            mDrawable.setBounds(x, y, x + width, y + height);
+            mDrawable.draw(canvas);
+            invalidate();
+        }
+    }
+
+}
