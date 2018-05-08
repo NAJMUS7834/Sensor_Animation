6e34e89b257422353710dcbc3a6c938add987a64
Najmus_Seemab <najmus.seemab@gmail.com>
2018-05-08 20:42:58 +0530

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/najmus/useaccelerometer/ExampleUnitTest.java
+++ app/src/test/java/com/example/najmus/useaccelerometer/ExampleUnitTest.java
@@ -1,4 +1,7 @@
 package com.example.najmus.useaccelerometer;
+/*====================================
+    Author : NAJMUS SEEMAB
+======================================*/
 
 import org.junit.Test;
 
